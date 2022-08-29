b9b456bdb284a5eafbff0dd6e20e60a8eacefff7
onozeam <maezonoinfo@gmail.com>
2022-08-29 15:37:46 +0900

    feat: binary search

--- hello.go
+++ hello.go
@@ -4,18 +4,22 @@ import "fmt"
 
 // 二分探索
 func main() {
-	var target = 2
-	var arr = [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	var target = 8
+	var arr = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	var center = len(arr) / 2
-	fmt.Println(arr[center])
+	fmt.Println(arr, center)
 	for true {
 		if target > arr[center] {
-			fmt.Println(">")
+			arr = arr[center:]
+			center = len(arr) / 2
+			fmt.Println(arr, center)
 		} else if target < arr[center] {
-			fmt.Println("<")
+			arr = arr[:center]
+			center = len(arr[:center]) / 2
+			fmt.Println(arr, center)
 		} else {
-			fmt.Println("o")
+			fmt.Println("found", arr[center])
+			break
 		}
-		break
 	}
 }
